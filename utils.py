ed4acf842a5896a3a61311931195d334d1fd8512
joeng03 <yinjoeng2003@gmail.com>
2024-07-03 21:00:30 +0800

    checkpoint: text to audio

--- utils.py
+++ utils.py
@@ -1,5 +1,10 @@
 import ffmpeg
 import io
+from TTS.api import TTS
+import torch
+
+device = "cuda" if torch.cuda.is_available() else "cpu"
+tts = TTS("tts_models/multilingual/multi-dataset/xtts_v2").to(device)
 
 
 # def video_to_audio(input_file, output_file_path):
@@ -34,3 +39,7 @@ def video_to_audio(input_bytes: bytes) -> bytes:
         raise ffmpeg.Error('ffmpeg', process.returncode, process.stderr)
     
     return stdout
+
+def text_to_audio(text: str, language: str, speaker_wav: str):
+    tts.tts_to_file(text=text, language=language, speaker_wav=speaker_wav, file_path="temp/output.wav")
+    
